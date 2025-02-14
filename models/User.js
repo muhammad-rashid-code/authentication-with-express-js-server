d562415ec3da8d7aa1cedd7a09605d066b4f8ff0
muhammad rashid <m.rashid212823@gmail.com>
2025-02-14 12:52:25 +0500

    Good to go

---
+++ models/User.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+});
+
+const User = mongoose.model("User", taskSchema);
+export default User;
