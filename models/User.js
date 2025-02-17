5165154ed6943c9893a3d60e82901d609a475197
muhammad rashid <m.rashid212823@gmail.com>
2025-02-17 05:51:31 +0500

    timestamps: true

--- models/User.js
+++ models/User.js
@@ -1,12 +1,15 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  fullname: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-});
+const userSchema = new Schema(
+  {
+    fullname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
